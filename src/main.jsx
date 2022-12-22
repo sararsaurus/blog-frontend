f7b9ace0e4ccc6430fe8a8fb49c3bd0815c49431
Sara Raley <sara.m.raley@gmail.com>
2022-12-22 13:02:18 -0700

    updates CORS

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 import "./index.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://peppy-kitsune-4b4350.netlify.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
